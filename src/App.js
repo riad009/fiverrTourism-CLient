b4ae2062c321761a1c4d8084df898be65338a0f1
riad009 <riadhossinr4@gmai.com>
2022-12-29 18:20:45 +0600

    add code

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+         ok good
         </a>
       </header>
     </div>
---
+++ src/firebase.config.js
@@ -0,0 +1,18 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyA3KGDcDiBedo1zzrsuM_lDATPIe29mpB8",
+  authDomain: "task-68a9a.firebaseapp.com",
+  projectId: "task-68a9a",
+  storageBucket: "task-68a9a.appspot.com",
+  messagingSenderId: "562250271852",
+  appId: "1:562250271852:web:f5dcf889ad4ea3d073dc49"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+export default app;
